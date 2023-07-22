7db77e6e4eb2c203eb616923deef529fa65b1c2e
mark-kibo <kibochamark@gmail.com>
2023-07-22 16:18:20 +0300

    build:created fetch requests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 let addToy = false;
+let toyDiv = document.getElementById('toy-collection')
+let form = document.querySelector("form");
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -12,4 +16,153 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+
+  // add card  to our toy div
+  let toys = getData()
+  toys.then(data => {
+    data.map((toy) => {
+      let toyCard = document.createElement('div');
+      toyCard.setAttribute('class', 'card')
+      toyCard.innerHTML = `
+    <h2>${toy.name}</h2>
+    <img src="${toy.image}" class="toy-avatar" />
+    <p>${toy.likes} Likes</p>
+    <button class="like-btn" id="${toy.id}">Like ❤️</button>
+    `
+      toyDiv.appendChild(toyCard);
+      //  like image listener
+      const likeButton = toyCard.querySelector('.like-btn');
+     likeButton.addEventListener("click",(e)=>{
+      let imageLike={"likes":toy.likes+1}
+      console.log(patchData(imageLike, e.target.id));
+     })
+    })
+
+  })
+
+  // create a toy
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let toyObject = {}
+    let nodeList = e.target.querySelectorAll("input.input-text");
+    nodeList.forEach(element => {
+      toyObject.name = element.name
+      toyObject.imageUrl = element.value
+    });
+    let toyCreate = postData(toyObject);
+    console.log(toyCreate);
+    form.reset();
+  })
+  
+
+  
+}
+);
+
+
+
+
+// get data from server
+const getData = async () => {
+  try {
+    // get data from server
+    const response = await fetch('http://localhost:3000/toys');
+    if (response.status != 200) {
+      throw new Error("No data");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+
+
+// add data to server
+const postData = async(body) => {
+  let data ={...body, likes:0}
+  console.log(data)
+  let passedData = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(data)
+  }
+  console.log(passedData)
+  try {
+    const response = await fetch('http://localhost:3000/toys', passedData);
+    if (response.status != 200) {
+      throw new Error("invalid data");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+
+}
+
+// update data to server
+const putData = async (body) => {
+  let passedData = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(body)
+  }
+  try {
+    const response = await fetch('', passedData);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+
+}
+
+// patch data to server(partially update)
+const patchData = async (body, id) => {
+  let passedData = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(body)
+  }
+  try {
+    const response = await fetch(`http://localhost:3000/toys/${id}`, passedData);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+
+}
+
+// delete data on server
+const DeleteData = async (id) => {
+  let passedData = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: {}
+  }
+  try {
+    const response = await fetch(`http://localhost:3000/toys/${id}`, { passedData });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
